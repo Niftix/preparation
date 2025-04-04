f087ca109cfc1020ee6ce755c35864c65cf66f56
Niftix <Mathdigitale@gmail.com>
2025-04-04 20:02:52 +0000

    Exo N1 avec Grok

---
+++ hello_piscine.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	char prenom[20] = "Mathieu";
+	printf("Hello, %s", prenom);
+	return 0;
+}
--- intmax.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef intmax_h
-#define intmax_h
-
-#include <stdio.h>
-
-void intmax(int a, int b);
-
-#endif
