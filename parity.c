65a7d5158a0a2d7041c347108194380fe9cd4361
Niftix <Mathdigitale@gmail.com>
2025-04-05 12:58:31 +0000

    update

--- parity.c
+++ parity.c
@@ -5,11 +5,11 @@ int main()
 	int nombre = 7;
 	int pair = 0;
 	int impair =1;
-	if(nombre % 2 < 0)	
+	if(nombre % 2 == 0)	
 	{
 		printf("Pair");
 	}
-	else(nombre % 2 > 0);
+	else(nombre % 2 != 0);
 	{
 		printf("Impair");
 	}
