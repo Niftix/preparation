4054e01ac2d4b64de8f2f0615a01acdfc464777d
Niftix <Mathdigitale@gmail.com>
2025-04-06 11:47:26 +0000

    swap

--- swap.c
+++ swap.c
@@ -2,13 +2,13 @@
 
 void swap(int* a, int* b);
 
-int main()
+int main(void)
 {
 	int a = 10;
 	int b = 5;
-	printf("a = %d, b = %d\n",a,b);
+	printf("Avant : %d %d\n",a,b);
 	swap(&a,&b);
-	printf("a = %d, b = %d\n",a,b);
+	printf("Apres : %d %d\n",a,b);
 	return 0;
 }
 
