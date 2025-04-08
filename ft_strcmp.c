5d3bf01436b29270b99668384c0e58d25251c1b7
Niftix <Mathdigitale@gmail.com>
2025-04-08 11:37:54 +0000

    d

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-int ft_strcat(const char* chaine1, const char* chaine2);
+int ft_strcmp(const char* chaine1, const char* chaine2);
 
 int main(void)
 {
 	char chaine1[] = "hello";
 	char chaine2[] = "hello";
-	int resultat = ft_strcat(chaine1, chaine2);
+	int resultat = ft_strcmp(chaine1, chaine2);
 	if(resultat == 0)
 	{
 		printf("Identiques\n");
@@ -18,16 +18,16 @@ int main(void)
 	return 0;
 }
 
-int ft_strcat(const char* chaine1, const char* chaine2)
+int ft_strcmp(const char* chaine1, const char* chaine2)
 {
 	int i = 0;
-	while(chaine1[i] == chaine2[i])
+	while(chaine1[i] == chaine2[i] && chaine1[i] != '\0' && chaine2[i] != '\0')
 	{
-		if(chaine1[i] != chaine2[i])
-		{
-			return 1;
-		}
 		i++;
+	}
+	if(chaine1[i] != chaine2[i])
+	{
+	return 1;
 	}
 	return 0;
 }
