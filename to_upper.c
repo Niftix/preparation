d7c5b39b5701cfd8b5ae73eacac98df434376225
Niftix <Mathdigitale@gmail.com>
2025-04-08 10:01:41 +0000

    to_upper

--- to_upper.c
+++ to_upper.c
@@ -6,16 +6,21 @@ int main(void)
 {
 	char chaine[] = "hello";
 	to_upper(chaine);
-	printf("%s",chaine);
+	printf("%s\n",chaine);
 	return 0;
 }
 
 void to_upper(char* chaine)
 {
+	int max = 122;
+	int min = 97;
 	int i = 0;
 	while(chaine[i] != '\0')
 	{
+		if(chaine[i] >= min && chaine[i] <= max)
+		{
 		chaine[i] -= 32;
+		}
 		i++;
 	}
 }
