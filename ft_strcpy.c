930b1c6d8ba5e27bce3fd3b14e4ccdb32b58ff5d
Niftix <Mathdigitale@gmail.com>
2025-04-08 10:48:29 +0000

    message

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
-void ft_strcpy(char* dest, char* src, int taillemax);
+void ft_strcpy(char* dest, char* src);
 
 int main(void)
 {
 	char dest[6]; 
 	char src[] = "Hello";
-	int taillemax = 6;
-	ft_strcpy(dest, src, taillemax);
+	ft_strcpy(dest, src);
 	printf("%s\n", dest);
 	return 0;
 }
 
-void ft_strcpy(char* dest, char* src, int taillemax)
+void ft_strcpy(char* dest, char* src)
 {
 	int i = 0;
-	while(src[i] != '\0' && i < taillemax)
+	while(src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
