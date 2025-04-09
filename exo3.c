99d3eeffdd7dba08bc97b77fb7dd2d843f0e1216
Niftix <Mathdigitale@gmail.com>
2025-04-09 14:58:34 +0000

    d

--- exo3.c
+++ exo3.c
@@ -2,5 +2,15 @@
 
 void ft_reverse_alphabet(void)
 {
+<<<<<<< HEAD
 	write(1,"zxywvutsrqpomnlkjihgfedcba",26);
+=======
+	char c = 'z';
+	while(c > 'a' - 1)
+	{
+	write(1,&c,1);
+	c--;
+	}
+>>>>>>> 5ae2f2f (d)
 }
+
