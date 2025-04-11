c5b0be1396d9c14ef015a2b9af1e6179fe5c1caa
Niftix <Mathdigitale@gmail.com>
2025-04-11 12:51:20 +0000

    d

--- exo6.c
+++ exo6.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 
 void ft_print_comb2(void);
-void ft_print(char d1, char u1, char d2, char u2);
+void ft_print(int d, int c);
 
 int main()
 {
@@ -9,14 +9,20 @@ int main()
 	return 0;
 }
 
-void ft_print(char d1, char u1, char d2, char u2)
+void ft_print(int d, int c)
 {
+	char d1,u1,d2,u2;
+	d1 = d / 10 + '0';
+	u1 = d % 10 + '0';
+	d2 = c / 10 + '0';
+	u2 = c % 10 + '0';
+
 		write(1,&d1,1);
 		write(1,&u1,1);
 		write(1," ",1);
 		write(1,&d2,1);
 		write(1,&u2,1);
-		if(!(d1 == '9' && u1 == '8' && d2 == '9' && u2 == '9'))
+		if(!(d1 == '9' && u1 == '8'&& d2 == '9' && u2 == '9'))
 		{
 		write(1,", ",1);
 		}
@@ -24,20 +30,14 @@ void ft_print(char d1, char u1, char d2, char u2)
 
 void ft_print_comb2(void)
 {
-	char d1,u1,d2,u2;
-	for(d1 = '0'; d1 <= '9'; d1++)
+	int d,c;
+	for(d = 0; d <= 99; d++)
 	{
-		for(u1 = '0'; u1 <= '9'; u1++)
-		{
-			for(d2 = '0'; d2 <= '9'; d2++)
+		for(c = 0; c <= 99; c++)
+		{	
+			if(d < c)
 			{
-				for(u2 = '0'; u2 <= '9'; u2++)
-				{		
-					if(d1 <= d2  && u1 < u2)
-					{
-						ft_print(d1,u1,d2,u2);
-					}
-				}
+				ft_print(d,c);
 			}
 		}
 	}
