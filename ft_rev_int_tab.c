c78f40ac0b8e075065100e01c0317a02d7b72aab
Niftix <Mathdigitale@gmail.com>
2025-04-12 11:40:32 +0000

    d

--- ft_rev_int_tab.c
+++ ft_rev_int_tab.c
@@ -1,20 +1,35 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 void ft_rev_int_tab(int *tab, int size);
 
 int main()
 {
-	int tableau[4]= {2,4,6,8};
+	int tableau[4] = {2,4,6,8};
 	int taille = 3;
 	ft_rev_int_tab(tableau, taille);
 	return 0;
 }
-
+*/
 void ft_rev_int_tab(int *tab, int size)
 {
-	while(size >= 0)
+
+	int tableau[4] = {210,410,5,9};
+	int i = 0;
+	int sizemove;
+	sizemove = size;
+	while(i <= size)
 	{
-		printf("%d", tab[size]);
-		size--;
+		tableau[i] = tab[sizemove];
+		i++;
+		sizemove--;
+
+	}
+	sizemove = 0;
+	i = size;
+	while(sizemove <= size)
+	{
+		tab[sizemove] = tableau[i];
+		i--;
+		sizemove++;
 	}
 }
