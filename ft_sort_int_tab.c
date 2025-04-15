60928385979550a55321d8007a9cf65659cde617
Niftix <Mathdigitale@gmail.com>
2025-04-15 16:07:28 +0000

    d

---
+++ ft_sort_int_tab.c
@@ -0,0 +1,26 @@
+void ft_sort_int_tab(int *tab, int size)
+{
+	int i = 0;
+	int x;
+	x = size - 1;
+	int stock;
+	int stack;
+
+	while(i <= size -1)
+	{
+		while(x >= i)
+		{
+			if(tab[i] > tab[x])
+			{
+				stock = tab[i];
+				stack = tab[x];
+				tab[i] = stack;
+				tab[x] = stock;
+				x = size;
+			}
+			x--;
+		}
+		i++;
+		x = size - 1;
+	}
+}
